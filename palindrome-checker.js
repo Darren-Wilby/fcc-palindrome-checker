92e4b846ef55350a022a42f13e63187c15543629
darren <darren.wilby10@gmail.com>
2023-05-29 12:03:24 +0100

    added comments

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,4 +1,6 @@
 function palindrome(str) {
+  // Converts string to array of chars, filters for only alphanumerics
+  // turns back into string and sets to lowercase
   const phrase = str
       .split("")
       .filter((match) => /[a-zA-Z0-9]/g.test(match))
@@ -6,10 +8,11 @@ function palindrome(str) {
       .toLowerCase();
 
   const len = phrase.length;
-
+  // Compares characters from start and end until converging in the middle
+  // Returns false as soon as error is found
   for (let i = 0, j = len - 1; i < len / 2; i++, j--) {
     if (phrase[i] !== phrase[j]) return false;
   }
-  
+
   return true;
 }
